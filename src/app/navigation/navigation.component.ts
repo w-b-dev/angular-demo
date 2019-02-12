163c7082ad881f8562a9b50efe4372d4ebbbb2dd
Daniel Santos <daniel@brasileiro.ca>
2019-02-12 17:58:02 -0500

    Adds simpler nav. Adds more images. TODO: high res should come from WP

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -4,9 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-navigation',
   template: `
     <nav>
-      <a routerLink="landing" routerLinkActive="active" class="nav-link">Good girls</a>
-      <a routerLink="other" routerLinkActive="active" class="nav-link">Bad apples</a>
-      <a routerLink="insistent" routerLinkActive="active" class="nav-link">Time travel issues...</a>
+      <a routerLink="landing" routerLinkActive="active" class="nav-link">The good ones</a>
+      <a routerLink="other" routerLinkActive="active" class="nav-link">Villains</a>
+      <!--<a routerLink="insistent" routerLinkActive="active" class="nav-link">Time travel issues...</a>-->
     </nav>
   `,
   styleUrls: ['./navigation.component.scss'],
