14c233fa451ca14c2ded952a692ea4585b366efc
Daniel Santos <daniel@brasileiro.ca>
2019-02-23 20:51:21 -0500

    Includes data property to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PageLandingComponent } from './page-landing/page-landing.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PageOtherComponent } from './page-other/page-other.component';
 
 const routes: Routes = [
-  { path: 'landing', component: PageLandingComponent },
-  { path: 'other', component: PageOtherComponent },
-  { path: '',
-    redirectTo: '/landing',
-    pathMatch: 'full'
+  {
+    path: 'landing',
+    component: PageLandingComponent,
+    data: {
+      animationFor: 'A-PageLandingComponent'
+    },
   },
-  { path: '**', component: PageNotFoundComponent }
+  {
+    path: 'other',
+    component: PageOtherComponent,
+    data: {
+      animationFor: 'B-PageOtherComponent'
+    },
+  },
+  {
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full',
+  },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
+/*For feature modules, call the RouterModule.forChild method to register additional routes.*/
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
