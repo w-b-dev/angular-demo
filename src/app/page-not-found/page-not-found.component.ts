f82a2a03c732fb9afee0f92f35d9329a693f9136
Daniel Santos <daniel@brasileiro.ca>
2019-02-24 00:36:32 -0500

    Fix 404 page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -4,64 +4,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-page-not-found',
   template: `
     <article class="not-found">
-      <h1 class="not-found--text">Can't say for sure what happened... 🤷‍♀️</h1>
+        <div class="not-found--text">🤷‍️</div>
+        <div class="not-found--text">Redirecting you in 3 seconds...</div>
     </article>
   `,
-  styles: [
-      `
-      :host {
-        overflow: hidden;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-image: url(../../assets/x-men/dark-phoenix--small.jpg);
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-      }
-
-      .not-found {
-        flex: 1;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      @keyframes not-found {
-        from {
-          opacity: 0.5;
-          /*transform: scale(2.0)*/
-        }
-        to {
-          opacity: 1;
-          /*transform: scale(0.8) rotateY(360deg);*/
-        }
-      }
-
-      .not-found--text {
-        font-size: 3rem;
-        padding: 1rem;
-        text-align: center;
-        text-shadow: 0 0 20px rgb(114, 114, 114);
-        color: white;
-        box-shadow: 0 0 20px 13px rgb(82, 59, 59);
-        background-color: crimson;
-        border-radius: 1rem;
-
-        /*
-        forwards - The element will retain the style values that is set by the last keyframe
-        */
-        animation-delay: 0ms;
-        animation-direction: normal;
-        animation-duration: 250ms;
-        animation-fill-mode: forwards;
-        animation-iteration-count: initial;
-        animation-play-state: running;
-        animation-timing-function: ease-in-out;
-        animation-name: not-found;
-      }
-    `,
-  ],
+  styleUrls: ['page-not-found.component.scss'],
 })
 export class PageNotFoundComponent implements OnInit {
 
