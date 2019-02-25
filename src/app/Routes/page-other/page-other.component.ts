18773694a4a0b2acf0bbdfca51ef04e845787497
Daniel Santos <daniel@brasileiro.ca>
2019-02-24 23:25:50 -0500

    Add geolocation support. Fix #31

--- src/app/Routes/page-other/page-other.component.ts
+++ src/app/Routes/page-other/page-other.component.ts
@@ -1,28 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationService } from '../../Shared/location.service';
+import { GoogleMapsGeoCodingResponse } from '../../Shared/types/GoogleMapsGeoCodingResponse';
 
 @Component({
   selector: 'app-page-other',
-  template: `
-    <article class="demo-article">
-      📈
-    </article>
-  `,
-  styles: [
-      `
-      :host {
-        display: flex;
-        height: 100%;
-        background-color: cornflowerblue;
-      }
-    `,
-  ],
+  templateUrl: 'page-other.component.html',
+  styleUrls: ['page-other.component.scss'],
 })
 export class PageOtherComponent implements OnInit {
+  GoogleMapsGeoCodingResponse: GoogleMapsGeoCodingResponse;
+  country: string;
 
-  constructor() {
+  constructor(private locationService: LocationService) {
   }
 
   ngOnInit() {
+    // When loading, acquire browser location
+    this.locationService.navigatorGeolocationGetCurrentPosition();
+    // Wait to ask Google?
+    setTimeout(this.getLocation.bind(this), 3000);
   }
 
+  getLocation(): void {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+    const lastElement = new RegExp('[a-zA-Z0-9]*$');
+    // Matches the last word on the string ('country')
+    if ( this.locationService.callGoogle() !== undefined ) {
+      this.locationService.callGoogle()
+        .subscribe(
+          x => {
+            this.GoogleMapsGeoCodingResponse = x;
+            if ( x ) {
+              this.country = lastElement
+                .exec(x.plus_code.compound_code)
+                .toString();
+            }
+          },
+          error1 => console.error(error1),
+          () => null,
+        );
+    }
+  }
 }
