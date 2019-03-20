e00a71e7922a90a2f1c7540cf5c766b24c7e17f9
Daniel Santos <daniel@codein.ca>
2019-03-20 01:59:13 -0400

    Consume wordpress (#45)
    * Initial commit towards issue #1
    
    * Consuming properly with typings. #1
    
    * Creates SCSS. Opens panel to display JSON results of API call #1
    
    * Restores default build process
    
    * Forces no zooming
    
    * Styling refactoring (initial design)
    
    * Allows only one selection at a time
    
    * Adds custom dynamic calls to all main 12ish routes

--- src/app/Routes/page-landing/page-landing.component.ts
+++ src/app/Routes/page-landing/page-landing.component.ts
@@ -1,25 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../../Shared/blog.service';
 
 @Component({
   selector: 'app-page-landing',
-  template: `
-    <article class="demo-article">
-      👍
-    </article>
-  `,
-  styles: [`
-    :host {
-      display: flex;
-      height: 100%;
-      background-color: antiquewhite;
-    }
-  `]
+  templateUrl: 'page-landing.component.html',
+  styleUrls: ['page-landing.component.scss'],
 })
 export class PageLandingComponent implements OnInit {
 
-  constructor() { }
+  results1: Array<string>;
+  results2: Array<string>;
 
-  ngOnInit() {
+  constructor(private blogService: BlogService) {
   }
 
+  ngOnInit() {
+    this.consumeService();
+    this.results2 = ['Click on the left to try it.'];
+  }
+
+  consumeService() {
+    this.results1 = this.blogService.filterMainEndpoints();
+  }
+
+  /*TODO: in a future update, save selections to the Store for state management*/
+  selectRoute(event: MouseEvent) {
+    // TS suggestions to use the explicit type assertion 'as'
+    const element: HTMLElement = event.target as HTMLElement;
+    this.filterActiveClass(element);
+    this.results2 = this.blogService.filterSpecificEndpoint(element.innerText);
+  }
+
+  filterActiveClass(element: HTMLElement) {
+    // Inspect all elements under the target's parent
+    /*TODO: fix type checking warning*/
+    // @ts-ignore
+    for ( const parentElementKey of element.parentElement.childNodes ) {
+      // If the element in the loop is the target, toggle active class
+      if ( parentElementKey === element ) {
+        element.classList.contains('active')
+          ? element.classList.remove('active')
+          : element.classList.add('active');
+      } else {
+        // If the element in the loop is NOT the target, remove active class
+        if ( parentElementKey.classList && parentElementKey.classList.contains('active') ) {
+          parentElementKey.classList.remove('active');
+        }
+      }
+    }
+  }
 }
---
+++ src/app/Shared/blog.service.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { WPRoutes } from './types/Wordpress';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BlogService {
+
+  ENDPOINT_VERSION = '/wp-json/wp/v2';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  filterMainEndpoints(): Array<string> {
+    const results: Array<string> = [];
+    this.getAllRoutes()
+      .subscribe(response => {
+        /* Note: https://www.typescriptlang.org/docs/handbook/iterators-and-generators.html
+         * Both for..of and for..in statements iterate over lists;
+         * the values iterated on are different though, for..in returns a list of keys on the object being iterated,
+         * whereas for..of returns a list of values of the numeric properties of the object being iterated. */
+        // Inspect every route and get the KEY
+        for ( let route in response.routes ) {
+          // Check suggested by TS compiler
+          if ( response.routes.hasOwnProperty(route) ) {
+            // console.log('Instance:', y);
+            const routeFullSegment = route.split('/wp/v2/');
+            // Check if there's actually any route -- because a[0] is always empty here
+            if ( routeFullSegment[1] ) {
+              // Splits the route in possibly multiple nested sections
+              const routeRelevantSegment = routeFullSegment[1].split('/');
+              // Get the first element (base route)
+              route = routeRelevantSegment[0];
+              // Check if values hasn't been added before
+              if ( results.indexOf(route) === -1 ) {
+                // Save the route in the array
+                results.push(route);
+              }
+            }
+          }
+        }
+      });
+    return results;
+  }
+
+  filterSpecificEndpoint(specificRoute: string): Array<string> {
+    const results: Array<any> = [];
+    this.getSpecificRoute(specificRoute)
+      .subscribe(response => {
+        if ( typeof response[Symbol.iterator] === 'function' ) {
+          // console.info('🔁' + specificRoute + '🔀 response is iterable');
+          if ( typeof response !== 'string' && response.length !== 0 ) {
+            /*TODO: check how to make response be recognized as iterable to avoid errors*/
+            /*TS2488: Type '{}' must have a '[Symbol.iterator]()' method that returns an iterator.*/
+            // @ts-ignore
+            for ( const route of response ) {
+              // console.log(route);
+              results.push(route);
+            }
+          } else {
+            response.length !== 0
+              ? results.push(response)
+              : results.push('Empty for now');
+          }
+        } else {
+          // console.info('➡' + specificRoute + '⬅ response is not iterable');
+          // console.table(response);
+          results.push(response);
+        }
+      });
+    return results;
+  }
+
+  private getAllRoutes() {
+    return this.httpClient.get<WPRoutes>(environment.blog + this.ENDPOINT_VERSION);
+  }
+
+  private getSpecificRoute(specificRoute: string) {
+    /*TODO: check how to process 400 status codes before crashing*/
+    if ( specificRoute === 'Settings'
+      || specificRoute === 'Themes'
+      || specificRoute === 'Users'
+      || specificRoute === 'Block-renderer' ) {
+      return new Observable<Array<any>>(x => x.next([specificRoute + ' requires authentication.']));
+    } else {
+      return this.httpClient.get<Array<any>>(environment.blog + this.ENDPOINT_VERSION + '/' + specificRoute);
+    }
+  }
+}
