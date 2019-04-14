62eeca69aae9089de48cb182205f5b9a3d976c11
Daniel Santos <daniel@brasileiro.ca>
2019-04-14 02:34:52 -0400

    Adds enum with some formula parameters

---
+++ src/app/Shared/types/CalculatorEnum.ts
@@ -0,0 +1,34 @@
+export enum CustomerType {
+  contractor = 1,
+  retail = 1.1,
+  dealer = 0.8,
+}
+
+export enum SkylightModel {
+  pvccm = 1,
+  pvcsr = 1.15,
+  ff = 1.2,
+  ffsr = 1.38
+}
+
+export enum GlazingType {
+  dome = 1,
+  glass = 1.2,
+}
+
+export enum FrameFinishing {
+  black = 1,
+  mill = 1,
+  brown = 1,
+}
+
+export enum NumberDomes {
+  single = 0.8,
+  double = 1,
+  triple = 1.45,
+}
+
+export enum TintedGlazing {
+  true = 1.15,
+  false = 1,
+}
